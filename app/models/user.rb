b9084cf949f9fecda138838a112d5d4d2b882f65
isaacpitwa <isaacpitwa256@gmail.com>
2022-08-02 11:32:39 +0300

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-    has_many :foods, dependent: :destroy
-    has_many :recipes, dependent: :destroy
-  
-    validates :name, presence: true
-  end
-  
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+
+  validates :name, presence: true
+end
