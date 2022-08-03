8ae38f9894b010828a33522340d7043116bd3289
amrendrakind <vision2010@gmail.com>
2022-08-03 21:29:13 +0530

    Update: Publics controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'publics/index'
-  # get 'shoppings/index'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -15,8 +12,5 @@ Rails.application.routes.draw do
     end
   resources :shoppings, only:[:index]
   resources :publics, only:[:index]
-
   end
-
-
 end
