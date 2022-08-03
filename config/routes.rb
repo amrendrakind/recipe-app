089f07121248ba6461f764af57e506ce662e7583
amrendrakind <vision2010@gmail.com>
2022-08-03 16:32:12 +0530

    Controllers structure created

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'users#index'
+  root 'user#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[new create destroy]
-      resources :likes, only: %i[new create]
+    resources :foods, only: %i[new create destroy]
+    resources :recipes, only: %i[index show new create destroy]  do
+      resources :recipes_foods, only: %i[index show]
     end
   end
+
+
 end
