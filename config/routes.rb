1bdc51cad956d804ada77bfa7b68eaa14ca0e7b8
amrendrakind <vision2010@gmail.com>
2022-08-03 15:39:49 +0530

    Update ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'users#index'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
+      resources :likes, only: %i[new create]
+    end
+  end
 end
