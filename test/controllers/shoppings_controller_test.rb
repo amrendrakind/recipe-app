927d0546b82578182c338821affd297a6bca9d44
amrendrakind <vision2010@gmail.com>
2022-08-03 20:27:44 +0530

    Add: Shopping controller

---
+++ test/controllers/shoppings_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ShoppingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get shoppings_index_url
+    assert_response :success
+  end
+end
