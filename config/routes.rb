1e8ed375d717da548c60133d04f5f0b645730235
amrendrakind <vision2010@gmail.com>
2022-08-03 20:40:07 +0530

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'shoppings/index'
+  # get 'shoppings/index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'user#index'
+  root 'users#index'
   resources :users, only: %i[index show] do
     resources :foods, only: %i[new create destroy]
     resources :recipes, only: %i[index show new create destroy]  do
       resources :recipes_foods, only: %i[index show]
     end
+  resources :shoppings, only:[:index]
+
   end
 
 
