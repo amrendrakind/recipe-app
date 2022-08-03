02176019795f11d07136acfba2e3c7c52f4140c8
amrendrakind <vision2010@gmail.com>
2022-08-03 17:54:43 +0530

    Update recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,31 @@
 class RecipesController < ApplicationController
   def index
-    @recipe = Recipe.all
+    @recipes = Recipe.all
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def new 
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe  = Recipe.new(recipe_params)
+    recipe.user_id = params[:user_id]
+
+    if recipe.save
+      redirect_to user_recipes_path(params[:user_id])
+    else
+      redirect_to new_user_recipe_path
+    end
+    
+    def destroy
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
   end
 end
