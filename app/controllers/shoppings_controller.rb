bc984cff8ca791219f1ad23c7ca0d23d59dcdbeb
amrendrakind <vision2010@gmail.com>
2022-08-03 20:28:19 +0530

    Linter error fixed

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,3 +1,3 @@
 class ShoppingsController < ApplicationController
-    def index; end
+  def index; end
 end
--- test/controllers/shoppings_controller_test.rb
+++ test/controllers/shoppings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ShoppingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get shoppings_index_url
     assert_response :success
   end
