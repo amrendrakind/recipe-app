672a230d82314a088dd89e17a0ce75f70bb40e38
amrendrakind <vision2010@gmail.com>
2022-08-04 17:58:27 +0530

    Update: Recipe show item

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,6 +6,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe_food = RecipeFood.where(recipe_id: params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :foods, except: %i[:edit]
     resources :recipes, except: %i[:edit]  do
-      resources :recipes_foods, only: %i[index show]
+      resources :recipes_foods, only: %i[new create destroy]
     end
   resources :shoppings, only:[:index]
   resources :publics, only:[:index]
