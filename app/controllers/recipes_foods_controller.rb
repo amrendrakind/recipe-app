c31426803ac81cf5c6edbe7976ad1eb272c7f379
amrendrakind <vision2010@gmail.com>
2022-08-04 20:24:24 +0530

    Update: action in recipe_foods controller

--- app/controllers/recipes_foods_controller.rb
+++ app/controllers/recipes_foods_controller.rb
@@ -1,4 +1,15 @@
 class RecipesFoodsController < ApplicationController
   def new
+    @foods = Food.all
+    @recipe_food = RecipeFood.new
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id]).destroy
+    redirect_to user_recipe_path(params[:user_id], params[:recipe_id])
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
