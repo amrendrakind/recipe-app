e3988793a54b184a2a4ad7689ae30dc88daf9088
amrendrakind <vision2010@gmail.com>
2022-08-05 20:31:43 +0530

    Update: Navigation menu

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,6 +1,6 @@
 class ShoppingsController < ApplicationController
   def index
-    puts params
+    # puts params
     @recipe_food = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
   end
 end
